9312fb3088ebced93536d99478ad21eca2bd61d8
back0893 <back0893@163.com>
2019-11-30 13:05:49 +0800

    ??上传频率决定上下线

--- client.php
+++ client.php
@@ -4,10 +4,15 @@
  * @version 1.0
  */
 
+$client=stream_socket_client('tcp://120.132.93.231:8084');
 //$client=stream_socket_client('tcp://106.75.154.221:8350');
-$client=stream_socket_client('tcp://127.0.0.1:10034');
-fwrite($client,'{G01:869975034791700:A0:B1:C00001:D10000:3700:19:30:01:00000B}');
-echo fread($client,30);
+//$client=stream_socket_client('tcp://127.0.0.1:10034');
+while(1){
+    fwrite($client,'{G01:869975034791700:A1:B1:C01075:D44816:2419:27:30:01:201138}');
+    echo fread($client,30);
+
+    sleep(5);
+}
 
 //fwrite($client,'{A:30:01:P1000H4000L1000}');
 //echo fread($client,5);
